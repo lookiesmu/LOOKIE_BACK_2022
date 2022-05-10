027ba97a1d9691d096fc5f7225e026736c61301a
JYLeem <jinyoungim105@gmail.com>
2022-05-10 17:39:11 +0900

    Week4

--- jpashop/src/test/java/com/example/lookie/LookieApplicationTests.java
+++ jpashop/src/test/java/com/example/lookie/LookieApplicationTests.java
@@ -1,10 +1,10 @@
-package jpabook.jpashop;
+package com.example.lookie;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JpashopApplicationTests {
+class LookieApplicationTests {
 
 	@Test
 	void contextLoads() {
