98b63156a542a84c6d265a3a9dd900beebb1ee36
JYLeem <102199863+JYLeem@users.noreply.github.com>
2022-05-14 01:32:27 +0900

    Delete domain directory

--- domain/memberAccess.java
+++
@@ -1,6 +0,0 @@
-package jpabook.jpashop.domain;
-
-public enum memberAccess {
-
-    ACCEPT, DECLINE
-}
--- domain/questionEdit.java
+++
@@ -1,7 +0,0 @@
-package jpabook.jpashop.domain;
-
-public enum questionEdit {
-
-    EDIT, DELETE, ADD
-
-}
