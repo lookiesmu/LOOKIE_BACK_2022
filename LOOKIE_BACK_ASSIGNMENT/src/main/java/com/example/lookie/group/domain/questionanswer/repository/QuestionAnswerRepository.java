9a94660abc7cacd874a3c3b1d323ed6c16318a6c
Dongmin-Kim <rkdlem48@gmail.com>
2022-05-22 15:24:44 +0900

    Revert "임진영 - 4주차 과제 제출"

--- LOOKIE_BACK_ASSIGNMENT/src/main/java/com/example/lookie/group/domain/questionanswer/repository/QuestionAnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.lookie.group.domain.questionanswer.repository;
-
-import com.example.lookie.group.domain.QuestionAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionAnswerRepository extends JpaRepository<QuestionAnswer, Long> {
-}
